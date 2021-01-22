cd62f3f0cd25f8ae524d3084597dffd6d1c6485a
Charlie Newey <charlie@newey.me>
2021-01-22 17:53:40 +0000

    Finish profile entry UIs

--- salt-solver/src/App.js
+++ salt-solver/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
@@ -19,11 +19,20 @@ import MineralEntry from './MineralEntry';
 import { WaterProfilesTable } from './DataTables';
 import Review from './Review';
 
+const roWater = {
+  calcium: "1",
+  magnesium: "0",
+  sodium: "0",
+  sulfate: "0",
+  chloride: "4",
+  bicarbonate: "16"
+};
+
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color="inherit" href="https://github.com/charlienewey/">
         Charles Newey
       </Link>{' '}
       {new Date().getFullYear()}
@@ -32,10 +41,7 @@ function Copyright() {
   );
 }
 
-const useStyles = makeStyles((theme) => ({
-  appBar: {
-    position: 'relative',
-  },
+const styles = (theme) => ({
   layout: {
     width: 'auto',
     marginLeft: theme.spacing(2),
@@ -67,124 +73,181 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(3),
     marginLeft: theme.spacing(1),
   },
-}));
+});
 
-const steps = ['Source profile input', 'Target profile input', 'Review your changes'];
+class SaltSolverApp extends React.Component  {
+  steps = ['Source profile input', 'Target profile input', 'Solve', 'Review your changes'];
 
-function getStepContent(step, selectRow) {
-  switch (step) {
-    case 0:
-      return (
-        <React.Fragment>
-          <Typography variant="h6" gutterBottom>
-            Source Water Profile Input
-          </Typography>
-          <br />
-          <MineralEntry classNames='SourceProfileEntry'></MineralEntry>
-        </React.Fragment>
-      );
-    case 1:
-      return (
-        <React.Fragment>
-          <Typography variant="h6" gutterBottom>
-            Target Water Profile Input
-          </Typography>
-          <br />
-          <MineralEntry classNames='TargetProfileEntry'></MineralEntry>
-          <br />
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel2a-content"
-              id="panel2a-header"
-            >
-              Water Profile Presets
-            </AccordionSummary>
-            <AccordionDetails>
-              <WaterProfilesTable
-                selectRow={selectRow}
-              >
-              </WaterProfilesTable>
-            </AccordionDetails>
-          </Accordion>
-        </React.Fragment>
-      );
-    case 2:
-      return <Review />;
-    default:
-      throw new Error('Unknown step');
+  constructor (props) {
+    super(props);
+    this.classes = props.classes;
+
+
+    if (props.state === undefined || props.state === null) {
+      const defaultSourceProfile = {...roWater};
+      const defaultTargetProfile = {...roWater};
+
+      this.state = {
+        activeStep: 0,
+        sourceProfile: defaultSourceProfile,
+        targetProfile: defaultTargetProfile 
+      };
+    } else {
+      this.state = props.state;
+    };
+ 
+    // water profile state
+    this.handleSourceProfileChange = this.handleSourceProfileChange.bind(this);
+    this.handleTargetProfileChange = this.handleTargetProfileChange.bind(this);
+    this.selectPresetTarget = this.selectPresetTarget.bind(this);
+
+    // pagination state controls
+    this.handleNext = this.handleNext.bind(this);
+    this.handleBack = this.handleBack.bind(this);
+  };
+
+  componentDidUpdate (prevProps) {
+    if (prevProps.state !== this.props.state) {
+      this.setState(this.props.state);
+    };
+  };
+
+  handleSourceProfileChange (amount, mineral) {
+    var sourceProfile = this.state.sourceProfile || {}; 
+    sourceProfile[mineral] = amount;
+    this.setState({sourceProfile});
+  };
+
+  handleTargetProfileChange (amount, mineral) {
+    var targetProfile = this.state.targetProfile || {}; 
+    targetProfile[mineral] = amount;
+    this.setState({targetProfile});
+  };
+
+  selectPresetTarget (row) {
+    this.setState({targetProfile: row});
+  };
+
+  handleNext () {
+    this.setState({activeStep: this.state.activeStep + 1});
+  };
+
+  handleBack () {
+    this.setState({activeStep: this.state.activeStep - 1});
+  };
+
+  getStepContent(step) {
+    switch (step) {
+      case 0:
+        return (
+          <React.Fragment>
+            <Typography variant="h6" gutterBottom>
+              Source Water Profile Input
+            </Typography>
+            <br />
+            <MineralEntry
+              classNames='sourceProfile'
+              state={this.state.sourceProfile}
+              onMineralChange={this.handleSourceProfileChange}>
+            </MineralEntry>
+          </React.Fragment>
+        );
+      case 1:
+        return (
+          <React.Fragment>
+            <Typography variant="h6" gutterBottom>
+              Target Water Profile Input
+            </Typography>
+            <br />
+            <div>
+              <MineralEntry
+                classNames='targetProfile'
+                state={this.state.targetProfile}
+                onMineralChange={this.handleTargetProfileChange}>
+              </MineralEntry>
+            </div>
+            <br />
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2a-content"
+                id="panel2a-header">
+                Water Profile Presets
+              </AccordionSummary>
+              <AccordionDetails>
+                <WaterProfilesTable
+                  selectRow={this.selectPresetTarget}>
+                </WaterProfilesTable>
+              </AccordionDetails>
+            </Accordion>
+          </React.Fragment>
+        );
+      case 2:
+        console.log(this.state);
+        return <h1>HI</h1>;
+      case 3:
+        return <Review />;
+      default:
+        throw new Error('Unknown step');
+    }
+  };
+
+  render () {
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <main className={this.classes.layout}>
+          <Paper className={this.classes.paper}>
+            <Typography component="h1" variant="h4" align="center">
+              Brewing Salt Calculator
+            </Typography>
+
+            <Stepper activeStep={this.state.activeStep} className={this.classes.stepper}>
+              {this.steps.map((label) => (
+                <Step key={label}>
+                  <StepLabel>{label}</StepLabel>
+                </Step>
+              ))}
+            </Stepper>
+
+            <React.Fragment>
+              {this.state.activeStep === this.steps.length ? (
+                <React.Fragment>
+                  <Typography variant="h5" gutterBottom>
+                    Thank you for your order.
+                  </Typography>
+                  <Typography variant="subtitle1">
+                    Your order number is #2001539. We have emailed your order confirmation, and will
+                    send you an update when your order has shipped.
+                  </Typography>
+                </React.Fragment>
+              ) : (
+                <React.Fragment>
+                  {this.getStepContent(this.state.activeStep, this.selectRow)}
+                  <div className={this.classes.buttons}>
+                    {this.state.activeStep !== 0 && (
+                      <Button onClick={this.handleBack} className={this.classes.button}>
+                        Back
+                      </Button>
+                    )}
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={this.handleNext}
+                      className={this.classes.button}
+                    >
+                      {this.state.activeStep === this.steps.length - 1 ? 'Print Summary' : 'Next'}
+                    </Button>
+                  </div>
+                </React.Fragment>
+              )}
+            </React.Fragment>
+          </Paper>
+          <Copyright />
+        </main>
+      </React.Fragment>
+    );
   }
 }
 
-export default function App() {
-  const classes = useStyles();
-  const [activeStep, setActiveStep] = React.useState(0);
-
-  const selectRow = (row) => {
-    alert(row);
-    // this.setState({clickedRow: row});
-  };
-
-  const handleNext = () => {
-    setActiveStep(activeStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep(activeStep - 1);
-  };
-
-  return (
-    <React.Fragment>
-      <CssBaseline />
-      <main className={classes.layout}>
-        <Paper className={classes.paper}>
-          <Typography component="h1" variant="h4" align="center">
-            Brewing Salt Calculator
-          </Typography>
-
-          <Stepper activeStep={activeStep} className={classes.stepper}>
-            {steps.map((label) => (
-              <Step key={label}>
-                <StepLabel>{label}</StepLabel>
-              </Step>
-            ))}
-          </Stepper>
-
-          <React.Fragment>
-            {activeStep === steps.length ? (
-              <React.Fragment>
-                <Typography variant="h5" gutterBottom>
-                  Thank you for your order.
-                </Typography>
-                <Typography variant="subtitle1">
-                  Your order number is #2001539. We have emailed your order confirmation, and will
-                  send you an update when your order has shipped.
-                </Typography>
-              </React.Fragment>
-            ) : (
-              <React.Fragment>
-                {getStepContent(activeStep, this.selectRow)}
-                <div className={classes.buttons}>
-                  {activeStep !== 0 && (
-                    <Button onClick={handleBack} className={classes.button}>
-                      Back
-                    </Button>
-                  )}
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={handleNext}
-                    className={classes.button}
-                  >
-                    {activeStep === steps.length - 1 ? 'Print Summary' : 'Next'}
-                  </Button>
-                </div>
-              </React.Fragment>
-            )}
-          </React.Fragment>
-        </Paper>
-        <Copyright />
-      </main>
-    </React.Fragment>
-  );
-}
+export default withStyles(styles)(SaltSolverApp);
--- salt-solver/src/MineralEntry.js
+++ salt-solver/src/MineralEntry.js
@@ -9,42 +9,19 @@ class MineralEntry extends React.Component {
   constructor(props) {
     super(props);
     this.classNames = props.classNames;
+    this.handleMineralChange = this.handleMineralChange.bind(this);
+  };
 
-    if (props.clickedRow === undefined || props.clickedRow === null) {
-      this.state = {
-        calcium: 0.0,
-        magnesium: 0.0,
-        sodium: 0.0,
-        sulfate: 0.0,
-        chloride: 0.0
-      };
+  handleMineralChange (event, mineral) {
+    if (this.props.onMineralChange !== undefined && this.props.onMineralChange !== null) {
+      this.props.onMineralChange(event.target.value, mineral);
     } else {
-      this.state = props.clickedRow
-    };
-
-    this.handleSingleChange = this.handleSingleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  };
-
-  componentDidUpdate (prevProps) {
-    if (prevProps.state !== this.props.state) {
-      this.setState(this.props.state);
-    };
-  };
-
-  handleSingleChange (event, state_target) {
-    var update = {};
-    update[state_target] = event.target.value;
-    this.setState(update);
-  };
-
-  handleSubmit (event) {
-    console.log(this.state);
-    event.preventDefault();
+      throw new Error('Missing onMineralChange prop');
+    }
   };
 
   render () {
-    let classNames = [this.classNames, 'MineralEntry'].join(' ')
+    const classNames = [this.classNames, 'MineralEntry'].join(' ')
     return (
       <Grid container justify='center' className={classNames} spacing={1}>
         <Grid item xs={12} sm={6} md={4} lg={2}>
@@ -52,8 +29,8 @@ class MineralEntry extends React.Component {
             label='Calcium'
             type='number'
             min={0.0}
-            value={this.state.calcium}
-            onChange={(e) => this.handleSingleChange(e, 'calcium')}
+            value={this.props.state.calcium}
+            onChange={(e) => this.handleMineralChange(e, 'calcium')}
             InputProps={{ startAdornment: <InputAdornment position="start">ppm</InputAdornment> }}
             variant="outlined"
             required
@@ -66,8 +43,8 @@ class MineralEntry extends React.Component {
             label='Magnesium'
             type='number'
             min={0.0}
-            value={this.state.magnesium}
-            onChange={(e) => this.handleSingleChange(e, 'magnesium')}
+            value={this.props.state.magnesium}
+            onChange={(e) => this.handleMineralChange(e, 'magnesium')}
             InputProps={{ startAdornment: <InputAdornment position="start">ppm</InputAdornment> }}
             variant="outlined"
             required
@@ -80,8 +57,8 @@ class MineralEntry extends React.Component {
             label='Sodium'
             type='number'
             min={0.0}
-            value={this.state.sodium}
-            onChange={(e) => this.handleSingleChange(e, 'sodium')}
+            value={this.props.state.sodium}
+            onChange={(e) => this.handleMineralChange(e, 'sodium')}
             InputProps={{ startAdornment: <InputAdornment position="start">ppm</InputAdornment> }}
             variant="outlined"
             required
@@ -94,8 +71,8 @@ class MineralEntry extends React.Component {
             label='Sulfate'
             type='number'
             min={0.0}
-            value={this.state.sulfate}
-            onChange={(e) => this.handleSingleChange(e, 'sulfate')}
+            value={this.props.state.sulfate}
+            onChange={(e) => this.handleMineralChange(e, 'sulfate')}
             InputProps={{ startAdornment: <InputAdornment position="start">ppm</InputAdornment> }}
             variant="outlined"
             required
@@ -108,8 +85,22 @@ class MineralEntry extends React.Component {
             label='Chloride'
             type='number'
             min={0.0}
-            value={this.state.chloride}
-            onChange={(e) => this.handleSingleChange(e, 'chloride')}
+            value={this.props.state.chloride}
+            onChange={(e) => this.handleMineralChange(e, 'chloride')}
+            InputProps={{ startAdornment: <InputAdornment position="start">ppm</InputAdornment> }}
+            variant="outlined"
+            required
+            fullWidth
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={4} lg={2}>
+          <TextField
+            label='Bicarbonate'
+            type='number'
+            min={0.0}
+            value={this.props.state.bicarbonate}
+            onChange={(e) => this.handleMineralChange(e, 'bicarbonate')}
             InputProps={{ startAdornment: <InputAdornment position="start">ppm</InputAdornment> }}
             variant="outlined"
             required
@@ -119,6 +110,7 @@ class MineralEntry extends React.Component {
       </Grid>
     );
   }
-}
+};
+
 
 export default MineralEntry;
